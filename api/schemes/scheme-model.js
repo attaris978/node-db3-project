7939db3da94ec68d66fb3a7bd1ae106d32bd9cf4
unknown <ryancare@protonmail.com>
2022-05-10 10:36:04 -0500

    Added getSteps model function

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -129,7 +129,7 @@ const rowsMap = rows.map(step => {
   */
 }
 
-function findSteps(scheme_id) { // EXERCISE C
+async function findSteps(scheme_id) { // EXERCISE C
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
@@ -150,15 +150,39 @@ function findSteps(scheme_id) { // EXERCISE C
         }
       ]
   */
+      const rows = await db('schemes as sc')
+      .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+      .select('st.*', 'sc.scheme_name')
+      .where('sc.scheme_id', scheme_id)
+      .orderBy('st.step_number')
+
+      const name = await db('schemes as sc')
+      .select('sc.scheme_name')
+      .where('sc.scheme_id', scheme_id)
+
+      const scheme_name = name[0].scheme_name
+
+      const rowsMap = rows.map(step => {
+        const {step_id, step_number, instructions} = step;
+        return {
+          step_id,
+          step_number,
+          instructions,
+          scheme_name
+        }})
+        console.log(rowsMap);
+      
+        return rowsMap;
+
 }
 
-function add(scheme) { // EXERCISE D
+async function add(scheme) { // EXERCISE D
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
 }
 
-function addStep(scheme_id, step) { // EXERCISE E
+async function addStep(scheme_id, step) { // EXERCISE E
   /*
     1E- This function adds a step to the scheme with the given `scheme_id`
     and resolves to _all the steps_ belonging to the given `scheme_id`,
